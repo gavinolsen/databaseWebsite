c644727df58f26531da58c3b4c33e0780f62d77c
Gnarlygav <ols15015@byui.edu>
2018-12-08 15:06:27 -0700

    making some modifications to the keys file for admin users in the future

--- config/keys.js
+++ config/keys.js
@@ -5,7 +5,10 @@
 module.exports = {
   mongoURI:
     'mongodb://studentdb:F4nt4sticStud3nt7756@ds052837.mlab.com:52837/databasewebsite',
-  secretOrKey: 'secretOrKey'
+  secretOrKey: 'secretOrKey',
+  adminUsers: {
+    users: ['mclaughlinm@byui.edu']
+  }
 };
 
 //username studentdb
