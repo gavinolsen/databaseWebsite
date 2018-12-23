2f98bc0a93d4da25cba2966af4bf1d33e1497d27
Gnarlygav <ols15015@byui.edu>
2018-12-23 13:52:14 -0800

    updating getDay() gfunction

--- routes/api/stats.js
+++ routes/api/stats.js
@@ -595,6 +595,8 @@ router.get('/325', (req, res) => {
 
           console.log('getDay function in action!');
           console.log(getDay(request.date));
+          console.log('the date');
+          console.log(request.date);
 
           switch (getDay(request.date)) {
             case 2:
