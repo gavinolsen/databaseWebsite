09f5acb66f7cebe503a8318d1e4fb23034014336
Gnarlygav <ols15015@byui.edu>
2018-12-23 19:09:36 -0800

    adding the getDay to react components

--- server.js
+++ server.js
@@ -70,5 +70,4 @@ app.listen(port, () => console.log(`Server started on port ${port}`));
  *
  * another cool idea would be to list the users by descending number of requests
  * that would be in the stats
- *
  */
