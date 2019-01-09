cc213c14759ee6ca101ed0f769465503d40bc3fa
Gnarlygav <ols15015@byui.edu>
2019-01-08 20:01:20 -0700

    just added admin users

--- config/adminUsers.js
+++ config/adminUsers.js
@@ -4,7 +4,12 @@ module.exports = {
       'mclaughlinm@byui.edu',
       'enk13001@byui.edu',
       'drgmolsen@gmail.com',
-      'ols15015@byui.edu'
+      'ols15015@byui.edu',
+      'bul16005@byui.edu',
+      'lar17059@byui.edu',
+      'mun16012@byui.edu',
+      'loz16002@byui.edu',
+      'pay12008@byui.edu'
     ]
   }
 };
