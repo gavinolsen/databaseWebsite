ae83b296b1d4805a3c69ccc2c191495d7eb8d3e9
Gavin Olsen <ols15015@byui.edu>
2019-04-22 21:49:33 -0600

    took out redux dev tools and added the right admin users

--- config/adminUsers.js
+++ config/adminUsers.js
@@ -5,11 +5,11 @@ module.exports = {
       'enk13001@byui.edu',
       'drgmolsen@gmail.com',
       'ols15015@byui.edu',
-      'bul16005@byui.edu',
+      'ben15005@byui.edu',
       'lar17059@byui.edu',
-      'mun16012@byui.edu',
+      'hat16002@byui.edu',
       'loz16002@byui.edu',
-      'pay12008@byui.edu',
+      'pre15015@byui.edu',
       'johan.baer@walmart.com'
     ]
   }
