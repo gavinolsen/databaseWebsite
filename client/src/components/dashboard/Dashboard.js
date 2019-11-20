3015c922d0ec17a75b8f3a3d38d040ce7b9f91c5
Gavin Olsen <ols15015@byui.edu>
2019-11-19 23:03:46 -0600

    added description

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,20 +1,20 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import {Link, withRouter} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link, withRouter } from 'react-router-dom';
 
 //THIS COMPOENT SHOULD HAVE TWO LINKS TO THE LISTS
 
 class Dashboard extends Component {
-  componentWillMount () {
+  componentWillMount() {
     //console.log(this.props.auth.isAuthenticated);
     if (!this.props.auth.isAuthenticated) {
-      this.props.history.push ('/login');
+      this.props.history.push('/login');
     }
   }
 
-  render () {
-    const {user} = this.props.auth;
+  render() {
+    const { user } = this.props.auth;
 
     return (
       <div className="jumbotron">
@@ -26,6 +26,30 @@ class Dashboard extends Component {
           request help
         </Link>
         <hr className="my-4" />
+        <p>
+          Hey I'm Gavin Olsen. My LinkedIn information is at the bottom of the
+          page if you want to contact me.{' '}
+        </p>
+        <p>
+          Make a PR if you want any changes implemented to this website! Here's
+          the <a href="https://github.com/gavinolsen/databaseWebsite">
+            github
+          </a>. Clone it, make a new branch, implement changes, then push it
+          upstream. After pushing it upstream, go to the github link and create
+          a PR. I'll take a look at it, and approve it. It's a great opportunity
+          to learn new skills and develop for an open source project. Something
+          that employers look for on resumes are projects like this so get
+          involved and ask me about it! If you have any questions about setting
+          up the website on your own machine let me know and I'll help you out.
+        </p>
+        <p>
+          You should also be aware that I've left this website intentionally
+          suceptible to certain vulnerabilities, and it will be obvious what
+          those are if you look around the code. I've really thought of this
+          website as a playground, where not only I can learn, but you can learn
+          as well if you're willing to work and search through the details. But
+          please don't screw it up, for the sake of the class :)
+        </p>
         <p>
           thanks for being here, and let us know if there's anything we can do
         </p>
@@ -48,7 +72,6 @@ class Dashboard extends Component {
             <li />
           </ul>
         </div> */}
-
       </div>
     );
   }
@@ -58,8 +81,8 @@ Dashboard.propTypes = {
   auth: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-export default connect (mapStateToProps) (withRouter (Dashboard));
+export default connect(mapStateToProps)(withRouter(Dashboard));
